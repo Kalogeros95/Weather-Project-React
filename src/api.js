b441f4010ad73674faab143d0951e91586696454
Kalogeropoulos Athanasios <akalogeropoulos95@gmail.com>
2023-01-11 23:53:01 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '65dd2f4f34msh07fb5612c0fbb94p133dfejsn14acc5730083',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "13d8e2571a94eab7b6368901d7b87952";
